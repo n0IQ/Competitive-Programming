7ea8eedb1470dd2b43a3bdb0cd01912bf4c51a56
Mohit Varma <varmamohit122@gmail.com>
2020-12-07 23:15:00 +0530

    Add solution for problem `A - Avoid Trygub` Link: https://codeforces.com/contest/1450/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1450/submission/100648123

---
+++ codeforces/1450/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define vi vector<int>
+#define pii pair<int, int>
+#define all(x) x.begin(),x.end()
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+
+int main()
+{
+	fastio
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int T;
+	cin >> T;
+
+	while (T-- > 0)
+	{
+		int n;
+		cin >> n;
+
+		string s;
+		cin >> s;
+
+		sort(all(s));
+
+		cout << s << '\n';
+	}
+
+	return 0;
+}
