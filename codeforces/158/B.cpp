7fb575a799389c0dc776558cfdff78cd7cbea3e0
Mohit Varma <varmamohit122@gmail.com>
2021-01-16 03:54:00 +0530

    Add solution for problem `B - Taxi` Link: https://codeforces.com/contest/158/problem/B Tags: *special problem, greedy, implementation, *1100 Ref: https://codeforces.com/contest/158/submission/104451860

---
+++ codeforces/158/B.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define repR(i, n) for (int i = n-1; i >= 0; --i)
+#define repA(i, a, n) for (int i = a; i <= n; ++i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vd vector<double>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vpdd vector<pdd>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+
+int main()
+{
+	fastio
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+	cin >> n;
+
+	int count[5] = {0};
+
+	count[0] = 0;
+	rep(i, n) {
+		int num; cin >> num;
+		count[num]++;
+	}
+
+	ll taxi = count[4];
+
+	while (count[3] > 0 && count[1] > 0) {
+		count[3]--;
+		count[1]--;
+		taxi++;
+	}
+
+	while (count[3] > 0) {
+		count[3]--;
+		taxi++;
+	}
+
+	while (count[2] > 1) {
+		count[2] -= 2;
+		taxi++;
+	}
+
+	while (count[2] > 0 && count[1] > 0) {
+		count[2]--;
+		count[1] -= 2;
+		taxi++;
+	}
+
+	while (count[2] > 0) {
+		count[2] -= 2;
+		taxi++;
+	}
+
+	while (count[1] > 0) {
+		count[1] -= 4;
+		taxi++;
+	}
+
+	cout << taxi << '\n';
+
+	return 0;
+}
