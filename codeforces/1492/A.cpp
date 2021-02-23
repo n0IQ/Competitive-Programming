3a692afaea787b204ba0c9244ca10e7f787a8bc5
Mohit Varma <varmamohit122@gmail.com>
2021-02-24 03:12:00 +0530

    Add solution for problem `A - Three swimmers` Link: https://codeforces.com/contest/1492/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1492/submission/108343139

---
+++ codeforces/1492/A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define repR(i, n) for (int i = n-1; i >= 0; --i)
+#define repA(i, a, n) for (int i = a; i <= n; ++i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vd vector<double>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define vpdd vector<pdd>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	ll p, a, b, c;
+	cin >> p >> a >> b >> c;
+
+	a *= (p % a == 0 ? p / a : p / a + 1);
+	b *= (p % b == 0 ? p / b : p / b + 1);
+	c *= (p % c == 0 ? p / c : p / c + 1);
+
+	cout << min({a - p, b - p, c - p}) << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
