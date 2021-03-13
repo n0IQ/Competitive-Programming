537f746773ad29e1377daab61dc008e789c2e4d7
Mohit Varma <varmamohit122@gmail.com>
2021-03-14 03:17:00 +0530

    Add solution for problem `F - LCS` Link: https://atcoder.jp/contests/dp/tasks/dp_f Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/20914439

--- atcoder/dp/F.cpp
+++ atcoder/dp/F.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 
 #define pb push_back
-#define ppb pop_back
 #define mp make_pair
 #define lb lower_bound
 #define ub upper_bound
@@ -13,67 +9,43 @@ using namespace __gnu_pbds;
 #define ss second
 
 #define rep(i, a, b) for(int i = a; i < b; i++)
-#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) (int)(x).size()
-#define mem0(a) memset(a,0,sizeof(a))
 #define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
 
 typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-#define MOD1 1000000007
-#define MOD2 998244353
-#define INF  2000000000000000000
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
 #define ps(x,y) fixed << setprecision(y) << x
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
-template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-//member functions :
-//1. order_of_key(k) : number of elements strictly lesser than k
-//2. find_by_order(k) : k-th element in the set
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(int t) {cerr << t;}
-void _print(ll t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
-string print_lcs(string s, string t)
+string PrintLCS(string s1, string s2, int n, int m)
 {
-	int n = sz(s), m = sz(t);
 	int dp[n + 1][m + 1];
 
 	rep(i, 0, n + 1) {
 		rep(j, 0, m + 1) {
 			if (i == 0 || j == 0) dp[i][j] = 0;
-			else if (s[i - 1] == t[j - 1]) {
+			else if (s1[i - 1] == s2[j - 1]) {
 				dp[i][j] = 1 + dp[i - 1][j - 1];
 			}
 			else {
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
 			}
 		}
 	}
@@ -82,38 +54,42 @@ string print_lcs(string s, string t)
 	string lcs;
 
 	while (i > 0 && j > 0) {
-		if (s[i - 1] == t[j - 1]) {
-			lcs.pb(s[i - 1]);
+		if (s1[i - 1] == s2[j - 1]) {
+			lcs.pb(s1[i - 1]);
 			i--, j--;
 		}
 		else {
-			if (dp[i][j - 1] > dp[i - 1][j]) j--;
+			if (dp[i][j - 1] >= dp[i - 1][j]) j--;
 			else i--;
 		}
 	}
 
 	reverse(all(lcs));
+
 	return lcs;
 }
 
 void solve()
 {
-	string s, t;
-	cin >> s >> t;
-	cout << print_lcs(s, t) << '\n';
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	int n = s1.length(), m = s2.length();
+
+	cout << PrintLCS(s1, s2, n, m) << '\n';
 }
 
 int main()
 {
-	/*#ifndef ONLINE_JUDGE
-		freopen("input.txt", "r", stdin);
-		freopen("output.txt", "w", stdout);
-		freopen("Error.txt", "w", stderr);
-	#endif*/
-
 	fastio;
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
 	int t = 1;
-	//cin >> t;
+	//cin>>t;
 	while (t--) solve();
 
 	return 0;
