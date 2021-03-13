f9c1de6f4160284c1db44a255bb7577d8f25c657
Mohit Varma <varmamohit122@gmail.com>
2021-03-13 21:48:00 +0530

    Add solution for problem `B - BerSU Ball` Link: https://codeforces.com/contest/489/problem/B Tags: dfs and similar, dp, graph matchings, greedy, sortings, two pointers, *1200 Ref: https://codeforces.com/contest/489/submission/109909836

--- codeforces/489/B.cpp
+++ codeforces/489/B.cpp
@@ -34,11 +34,33 @@ typedef pair<ll, ll> pll;
 #define ps(x,y) fixed << setprecision(y) << x
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
 
+int n, m;
 int dp[101][101];
+
+int bersu(int a[], int b[], int i, int j) {
+	if (i == n || j == m) {
+		return 0;
+	}
+
+	if (dp[i][j] != -1) {
+		return dp[i][j];
+	}
+
+	int p1 = 0, p2 = 0, p3 = 0;
+
+	if (abs(a[i] - b[j]) <= 1) {
+		p1 = 1 + bersu(a, b, i + 1, j + 1);
+	}
+
+	p2 = bersu(a, b, i, j + 1);
+	p3 = bersu(a, b, i + 1, j);
+
+	return dp[i][j] = max({p1, p2, p3});
+}
 
 void solve()
 {
-	int n, m;
+	//int n, m;
 	cin >> n;
 
 	int a[n];
@@ -51,18 +73,9 @@ void solve()
 	sort(a, a + n);
 	sort(b, b + m);
 
-	rep(i, 1, n + 1) {
-		rep(j, 1, m + 1) {
-			if (abs(a[i - 1] - b[j - 1]) <= 1) {
-				dp[i][j] = 1 + dp[i - 1][j - 1];
-			}
-			else {
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-			}
-		}
-	}
+	mem1(dp);
 
-	cout << dp[n][m] << '\n';
+	cout << bersu(a, b, 0, 0) << '\n';
 }
 
 int main()
