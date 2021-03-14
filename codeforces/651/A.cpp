2b1674a4421f77d319f3a1c89eb0f6df2b6a1015
Mohit Varma <varmamohit122@gmail.com>
2021-03-14 19:39:00 +0530

    Add solution for problem `A - Joysticks` Link: https://codeforces.com/contest/651/problem/A Tags: dp, greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/651/submission/109977904

---
+++ codeforces/651/A.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+int dp[1000][1000];
+
+int joysticks(int a, int b)
+{
+	if (a <= 0 || b <= 0) {
+		return 0;
+	}
+
+	if (a <= 1 && b <= 1) {
+		return 0;
+	}
+
+	if (dp[a][b] != -1) {
+		return dp[a][b];
+	}
+
+	return dp[a][b] = max(1 + joysticks(a - 2, b + 1), 1 + joysticks(a + 1, b - 2));
+}
+
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+
+	mem1(dp);
+
+	cout << joysticks(a, b) << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
