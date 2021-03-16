a2b6c10dbc96adfb78e416ae901ebe5f5bfd29c9
Mohit Varma <varmamohit122@gmail.com>
2021-03-16 23:02:00 +0530

    Add solution for problem `A - Ice Skating` Link: https://codeforces.com/contest/217/problem/A Tags: brute force, dfs and similar, dsu, graphs, *1200 Ref: https://codeforces.com/contest/217/submission/110134118

---
+++ codeforces/217/A.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+int x[101], y[101];
+bool visited[101];
+int n;
+
+void DFS(int s)
+{
+	visited[s] = true;
+
+	rep(i, 0, n) {
+		if (visited[i]) continue;
+
+		if (x[s] == x[i] || y[s] == y[i]) {
+			DFS(i);
+		}
+	}
+}
+
+void solve()
+{
+	cin >> n;
+
+	rep(i, 0, n) {
+		cin >> x[i] >> y[i];
+	}
+
+	int ans = 0;
+
+	rep(i, 0, n) {
+		if (!visited[i]) {
+			DFS(i);
+			ans++;
+		}
+	}
+
+	cout << ans - 1 << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
