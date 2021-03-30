0102f7d7d130ac53ece8ca58c08c32d72555664c
Mohit Varma <varmamohit122@gmail.com>
2021-03-31 01:57:00 +0530

    Add solution for problem `B - Petr and a Combination Lock` Link: https://codeforces.com/contest/1097/problem/B Tags: bitmasks, brute force, dp, *1200 Ref: https://codeforces.com/contest/1097/submission/111508657

---
+++ codeforces/1097/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+bool Petr(int a[], int n, int sum)
+{
+	if (n == 0) {
+		if (sum == 0 || sum % 360 == 0) return true;
+		else return false;
+	}
+
+	return Petr(a, n - 1, sum + a[n - 1]) || Petr(a, n - 1, sum - a[n - 1]);
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	int a[n];
+	rep(i, 0, n) cin >> a[i];
+
+	bool ok = Petr(a, n - 1, a[n - 1]) || Petr(a, n - 1, -1 * a[n - 1]);
+
+	if (ok) cout << "YES" << '\n';
+	else cout << "NO" << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
