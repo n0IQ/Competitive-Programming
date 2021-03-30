6b4be6e26b6522eab05a87afc84ed1135cc6edf1
Mohit Varma <varmamohit122@gmail.com>
2021-03-30 20:14:00 +0530

    Add solution for problem `B - Ternary String` Link: https://codeforces.com/contest/1354/problem/B Tags: binary search, dp, implementation, two pointers, *1200 Ref: https://codeforces.com/contest/1354/submission/111484507

---
+++ codeforces/1354/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	string s;
+	cin >> s;
+
+	int n = s.length(), one = -1, two = -1, three = -1, ans = INT_MAX;
+
+	rep(i, 0, n) {
+		if (s[i] == '1') one = i;
+		else if (s[i] == '2') two = i;
+		else three = i;
+
+		if (one != -1 && two != -1 && three != -1) {
+			int a = max({one, two, three});
+			int b = min({one, two, three});
+
+			ans = min(ans, a - b + 1);
+		}
+	}
+
+	(ans == INT_MAX ? cout << 0 : cout << ans);
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
