b5eadc2e3bc2e0b4848c4593c31e23dffcc3137f
Mohit Varma <varmamohit122@gmail.com>
2021-04-03 01:54:00 +0530

    Add solution for problem `A - Single Wildcard Pattern Matching` Link: https://codeforces.com/contest/1023/problem/A Tags: brute force, implementation, strings, *1200 Ref: https://codeforces.com/contest/1023/submission/111819622

---
+++ codeforces/1023/A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, m, indx = -1;
+	string s, t;
+	cin >> n >> m >> s >> t;
+
+	rep(i, 0, n) {
+		if (s[i] == '*') indx = i;
+	}
+
+	if (indx == -1) {
+		(s == t ? cout << "YES" : cout << "NO");
+		return;
+	}
+
+	int rem = m - (n - 1);
+	if (rem < 0) {
+		cout << "NO" << '\n';
+		return;
+	}
+
+	s = s.substr(0, indx) + t.substr(indx, rem) + s.substr(indx + 1, n - indx - 1);
+
+	s == t ? cout << "YES" : cout << "NO";
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
