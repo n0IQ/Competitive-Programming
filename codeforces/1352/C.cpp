a8c0ecc326ff505a35ae2032b217dee4e23b20d0
Mohit Varma <varmamohit122@gmail.com>
2021-04-11 17:46:00 +0530

    Add solution for problem `C - K-th Not Divisible by n` Link: https://codeforces.com/contest/1352/problem/C Tags: binary search, math, *1200 Ref: https://codeforces.com/contest/1352/submission/112651147

---
+++ codeforces/1352/C.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	ll n, k;
+	cin >> n >> k;
+
+	ll l = 1LL, r = 1000000000000000000, mid = 0;
+	while (l != r) {
+		mid = (l + r + 1LL) / 2;
+		ll cnt = mid - (mid / n);
+
+		if (cnt > k) {
+			r = mid - 1;
+		}
+		else if (cnt < k) {
+			l = mid + 1;
+		}
+		else {
+			l = mid;
+			break;
+		}
+	}
+
+	if (l % n == 0) {
+		l--;
+	}
+
+	cout << l << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
