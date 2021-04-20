6a69f03e7f82fd15167a9137262fddfa531d17a2
Mohit Varma <varmamohit122@gmail.com>
2021-04-21 02:40:00 +0530

    Add solution for problem `B - Equations of Mathematical Magic` Link: https://codeforces.com/contest/1064/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/1064/submission/113652345

---
+++ codeforces/1064/B.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+ll mult(ll a, ll b)
+{
+	ll res = (a * 1LL * b);
+	return res;
+}
+
+ll power(ll a, ll b)
+{
+	ll res = 1;
+
+	while (b) {
+		if (b % 2 == 1) {
+			res = mult(a, res);
+		}
+
+		a = mult(a, a);
+		b /= 2;
+	}
+
+	return res;
+}
+
+string decToBinary(int n)
+{
+	string s;
+
+	for (int i = 31; i >= 0; i--) {
+		int k = n >> i;
+		if (k & 1) s.pb('1');
+		else s.pb('0');
+	}
+
+	return s;
+}
+
+void solve()
+{
+	ll a;
+	cin >> a;
+
+	string s = decToBinary(a);
+	int cnt = 0;
+	rep(i, 0, sz(s)) {
+		if (s[i] == '1') cnt++;
+	}
+
+	cout << power(2, cnt) << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
