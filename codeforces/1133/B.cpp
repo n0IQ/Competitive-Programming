73c2214a1b634598b3bfe11bb15f0a6b58b4db2e
Mohit Varma <varmamohit122@gmail.com>
2021-04-22 22:43:00 +0530

    Add solution for problem `B - Preparation for International Women's Day` Link: https://codeforces.com/contest/1133/problem/B Tags: math, number theory, *1200 Ref: https://codeforces.com/contest/1133/submission/113909522

---
+++ codeforces/1133/B.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vi v(k);
+	rep(i, 0, n) {
+		int x; cin >> x;
+		v[x % k]++;
+	}
+
+	int ans = (v[0] / 2);
+
+	if (k % 2 == 0) ans += v[k / 2] / 2;
+
+	rep(i, 1, (k + 1) / 2) {
+		int j = k - i;
+		ans += min(v[i], v[j]);
+	}
+
+	cout << ans * 2 << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
