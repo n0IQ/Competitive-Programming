fc9b43a2bd9a528eb85534e26f1d9c86ccfb53ad
Mohit Varma <varmamohit122@gmail.com>
2021-04-27 21:25:00 +0530

    Add solution for problem `B - Christmas Spruce` Link: https://codeforces.com/contest/913/problem/B Tags: implementation, trees, *1200 Ref: https://codeforces.com/contest/913/submission/114380102

---
+++ codeforces/913/B.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+vvi adj;
+
+void solve()
+{
+	adj.clear();
+	int n;
+	cin >> n;
+	adj.resize(n + 1);
+
+	rep(i, 2, n + 1) {
+		int x; cin >> x;
+		adj[x].pb(i);
+	}
+
+	rep(i, 1, n + 1) {
+		if (sz(adj[i]) == 0) continue;
+		int cnt = 0;
+
+		for (auto v : adj[i]) {
+			if (sz(adj[v]) == 0) cnt++;
+		}
+
+		if (cnt < 3) {
+			cout << "No" << '\n';
+			return;
+		}
+	}
+
+	cout << "Yes" << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
