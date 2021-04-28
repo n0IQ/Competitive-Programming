6799e7951d44e2af4cbe4f3483564352cdedadfb
Mohit Varma <varmamohit122@gmail.com>
2021-04-28 18:10:00 +0530

    Add solution for problem `B - Polycarp's Practice` Link: https://codeforces.com/contest/1006/problem/B Tags: greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/1006/submission/114448831

---
+++ codeforces/1006/B.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+
+	vi v = a;
+	sort(all(v), greater<int>());
+	v.erase(v.begin() + k, v.end());
+
+	ll p = accumulate(all(v), 0LL);
+	vi ans(k, 0);
+	int j = 0;
+
+	rep(i, 0, n) {
+		auto it = find(all(v), a[i]);
+
+		if (it != v.end()) {
+			ans[j]++;
+			v.erase(it);
+			k--;
+			if (k > 0) j++;
+		}
+		else ans[j]++;
+	}
+
+	cout << p << '\n';
+	for (auto x : ans) cout << x << " ";
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
