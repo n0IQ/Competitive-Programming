53b4f7a8a735018d0c211a1657909b079bf74fcd
Mohit Varma <varmamohit122@gmail.com>
2021-05-04 14:19:00 +0530

    Add solution for problem `A - Avoiding Zero` Link: https://codeforces.com/contest/1427/problem/A Tags: math, sortings, *900 Ref: https://codeforces.com/contest/1427/submission/115106564

---
+++ codeforces/1427/A.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vi v1, v2;
+	rep(i, 0, n) {
+		int x; cin >> x;
+		if (x > 0)v1.pb(x);
+		else v2.pb(x);
+	}
+
+	sort(all(v1)); sort(all(v2));
+	int pos = accumulate(all(v1), 0);
+	int neg = accumulate(all(v2), 0);
+
+	if (abs(pos) > abs(neg)) {
+		cout << "YES" << '\n';
+		rep(i, 0, sz(v1)) cout << v1[i] << " ";
+		rep(i, 0, sz(v2)) cout << v2[i] << " ";
+		cout << '\n';
+	}
+	else if (abs(pos) < abs(neg)) {
+		cout << "YES" << '\n';
+		rep(i, 0, sz(v2)) cout << v2[i] << " ";
+		rep(i, 0, sz(v1)) cout << v1[i] << " ";
+		cout << '\n';
+	}
+	else cout << "NO" << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
