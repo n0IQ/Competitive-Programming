ae8d317dbaa0d63df86282fa8043f3f027205e9e
Mohit Varma <varmamohit122@gmail.com>
2021-05-07 15:15:00 +0530

    Add solution for problem `B - Coloring a Tree` Link: https://codeforces.com/contest/902/problem/B Tags: dfs and similar, dsu, greedy, *1200 Ref: https://codeforces.com/contest/902/submission/115498700

---
+++ codeforces/902/B.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+vvi adj;
+vi p, color;
+vb visited;
+
+void DFS(int s, int x)
+{
+	visited[s] = true;
+	color[s] = x;
+
+	for (auto v : adj[s]) {
+		if (!visited[v] && p[s] != v) {
+			DFS(v, x);
+		}
+	}
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	adj.resize(n + 1); color.assign(n + 1, 0), p.assign(n + 1, 0);
+	rep(i, 2, n + 1) {
+		int x; cin >> x;
+		adj[x].pb(i);
+		adj[i].pb(x);
+		p[i] = x;
+	}
+
+	int cnt = 0;
+
+	rep(i, 1, n + 1) {
+		int x; cin >> x;
+
+		if (color[i] != x) {
+			visited.assign(n + 1, false);
+			DFS(i, x);
+			cnt++;
+		}
+	}
+
+	cout << cnt << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
