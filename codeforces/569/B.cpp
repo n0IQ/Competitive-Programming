85d4bccae04dbda1c48608c3c8406ab3a27baf22
Mohit Varma <varmamohit122@gmail.com>
2021-05-08 22:15:00 +0530

    Add solution for problem `B - Inventory` Link: https://codeforces.com/contest/569/problem/B Tags: greedy, math, *1200 Ref: https://codeforces.com/contest/569/submission/115723623

---
+++ codeforces/569/B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vi a(n);
+	map<int, int> extras, mp;
+
+	rep(i, 0, n) {
+		cin >> a[i];
+	}
+
+	vi index , nums;
+
+	rep(i, 0, n) {
+		if (a[i] > n || mp.find(a[i]) != mp.end()) index.pb(i);
+		else mp[a[i]]++;
+	}
+
+	rep(i, 1, n + 1) {
+		if (mp.find(i) == mp.end()) nums.pb(i);
+	}
+
+	rep(i, 0, sz(index)) {
+		int t = index[i];
+		a[t] = nums[i];
+	}
+
+	rep(i, 0, n) cout << a[i] << " ";
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
