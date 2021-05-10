27fbb0240666cd5344449eaae0f9fe30b01ef6a3
Mohit Varma <varmamohit122@gmail.com>
2021-05-10 23:01:00 +0530

    Add solution for problem `B - M-arrays` Link: https://codeforces.com/contest/1497/problem/B Tags: constructive algorithms, greedy, math, *1200 Ref: https://codeforces.com/contest/1497/submission/115887025

---
+++ codeforces/1497/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	map<int, int> mp;
+	rep(i, 0, n) {
+		int x; cin >> x;
+		mp[x % k]++;
+	}
+
+	int cnt = 0;
+	for (auto it : mp) {
+		if (it.ff == 0) cnt++;
+		else if (2 * it.ff == k) cnt++;
+		else if (2 * it.ff < k || mp.find(k - it.ff) == mp.end()) {
+			int t1 = it.ss, t2 = mp[k - it.ff];
+			cnt += 1 + max(0, abs(t1 - t2) - 1);
+		}
+	}
+
+	cout << cnt << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
