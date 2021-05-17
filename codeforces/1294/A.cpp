e1b353affe0bfcc7559cb220cac1aa9cf2ed21cf
Mohit Varma <varmamohit122@gmail.com>
2021-05-17 22:25:00 +0530

    Add solution for problem `A - Collecting Coins` Link: https://codeforces.com/contest/1294/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1294/submission/116504084

---
+++ codeforces/1294/A.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int a, b, c, n;
+	cin >> a >> b >> c >> n;
+
+	int arr[3] = {a, b, c};
+	sort(arr, arr + 3);
+	a = arr[0], b = arr[1], c = arr[2];
+
+	n -= (c - a);
+	a += (c - a);
+	n -= (c - b);
+	b += (c - b);
+
+	if (n < 0 || n % 3 != 0) cout << "NO" << '\n';
+	else cout << "YES" << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
