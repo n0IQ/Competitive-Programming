d11790aab0fe08f5a899643f91d499cd3a7894c2
Mohit Varma <varmamohit122@gmail.com>
2021-05-28 02:12:00 +0530

    Add solution for problem `B - Equidistant String` Link: https://codeforces.com/contest/545/problem/B Tags: greedy, *1100 Ref: https://codeforces.com/contest/545/submission/117535860

---
+++ codeforces/545/B.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	string s, t;
+	cin >> s >> t;
+
+	int cnt = 0;
+	rep(i, 0, sz(s)) {
+		if (s[i] != t[i]) cnt++;
+	}
+
+	if (cnt % 2 == 1) {
+		cout << "impossible" << '\n';
+		return;
+	}
+
+	string ans;
+	int k = 0;
+	rep(i, 0, sz(s)) {
+		if (s[i] != t[i]) {
+			if (k < cnt / 2) ans.pb(s[i]);
+			else ans.pb(t[i]);
+
+			k++;
+		}
+		else ans.pb(s[i]);
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
