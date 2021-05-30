72a32e3d04731363d77bf37a1ba1d214d6059bc0
Mohit Varma <varmamohit122@gmail.com>
2021-05-31 02:48:00 +0530

    Add solution for problem `D1 - Sage's Birthday (easy version)` Link: https://codeforces.com/contest/1419/problem/D1 Tags: binary search, constructive algorithms, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1419/submission/117936859

---
+++ codeforces/1419/D1.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+	sort(all(a));
+
+	if (n == 2) {
+		cout << 0 << '\n';
+		rep(i, 0, n) cout << a[i] << " ";
+		return;
+	}
+
+	vi b;
+	rep(i, 0, n / 2) {
+		b.pb(a[n - i - 1]);
+		b.pb(a[i]);
+	}
+
+	if (n % 2 == 1) b.pb(a[n / 2]);
+	int ans = n / 2;
+	if (n % 2 == 0) ans--;
+	cout << ans << '\n';
+	rep(i, 0, n) cout << b[i] << " ";
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
