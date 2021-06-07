d08255919bddcf2eb32840187734c375b3377249
Mohit Varma <varmamohit122@gmail.com>
2021-06-07 15:12:00 +0530

    Add solution for problem `B - Prinzessin der Verurteilung` Link: https://codeforces.com/contest/1536/problem/B Tags: brute force, constructive algorithms, strings, *1200 Ref: https://codeforces.com/contest/1536/submission/118707650

---
+++ codeforces/1536/B.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	string s;
+	cin >> n >> s;
+
+	set<string> st;
+
+	rep(i, 0, n) {
+		string t;
+
+		for (int k = 0; k < 5 && i + k < n; k++) {
+			t.pb(s[i + k]);
+			st.insert(t);
+		}
+	}
+
+	rep(len, 1, 6) {
+		string t(len, 'a');
+
+		while (1) {
+			if (st.count(t) == 0) {
+				cout << t << '\n';
+				return;
+			}
+
+			int idx = len - 1;
+
+			while (idx >= 0 && t[idx] == 'z') {
+				t[idx] = 'a';
+				idx--;
+			}
+
+			if (idx < 0) break;
+			t[idx]++;
+		}
+	}
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
