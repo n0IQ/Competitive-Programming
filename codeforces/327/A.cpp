f90bb1af49c492cd34df6125f4f51fc1fe561277
Mohit Varma <varmamohit122@gmail.com>
2021-06-07 19:47:00 +0530

    Add solution for problem `A - Flipping Game` Link: https://codeforces.com/contest/327/problem/A Tags: brute force, dp, implementation, *1200 Ref: https://codeforces.com/contest/327/submission/118730861

---
+++ codeforces/327/A.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+
+	int ans = 0, cnt = 0;
+	rep(i, 0, n) {
+		rep(j, i, n) {
+			cnt = 0;
+
+			rep(k, 0, n) {
+				if (k >= i && k <= j) cnt += 1 - a[k];
+				else cnt += a[k];
+			}
+
+			ans = max(ans, cnt);
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
