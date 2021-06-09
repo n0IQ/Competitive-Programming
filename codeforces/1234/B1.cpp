f0942d9af76e17b645a2f18d31592957bf2477d8
Mohit Varma <varmamohit122@gmail.com>
2021-06-09 17:04:00 +0530

    Add solution for problem `B1 - Social Network (easy version)` Link: https://codeforces.com/contest/1234/problem/B1 Tags: implementation, *1000 Ref: https://codeforces.com/contest/1234/submission/118880323

---
+++ codeforces/1234/B1.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vi a(n);
+	rep(i, 0, n) cin >> a[i];
+
+	deque<int> d;
+	map<int, int> f;
+
+	rep(i, 0, n) {
+		if (f.count(a[i])) continue;
+		if (sz(f) == k) {
+			int t = d.back();
+			f.erase(t);
+			d.pop_back();
+			f[a[i]] = 1;
+			d.push_front(a[i]);
+		}
+		else {
+			f[a[i]] = 1;
+			d.push_front(a[i]);
+		}
+	}
+
+	cout << sz(d) << '\n';
+	for (auto it : d) cout << it << " ";
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
