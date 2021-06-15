4e75645b0e0be942ee243631657f57a5a71a9dee
Mohit Varma <varmamohit122@gmail.com>
2021-06-15 21:46:00 +0530

    Add solution for problem `D - Zero Remainder Array` Link: https://codeforces.com/contest/1374/problem/D Tags: math, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/1374/submission/119551071

---
+++ codeforces/1374/D.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> a(n);
+	rep(i, 0, n) cin >> a[i];
+
+	map<ll, ll> cnt;
+	ll mx = 0, ans = 0;
+	rep(i, 0, n) {
+		if (a[i] % k == 0) continue;
+
+		ll r = (a[i] + k - 1) / k;
+		ll t = k * r;
+		cnt[t - a[i]]++;
+		mx = max(mx, cnt[t - a[i]]);
+	}
+
+	for (auto [x, f] : cnt) {
+		if (f == mx) {
+			ans = x + (k * 1LL * (f - 1)) + 1;
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
