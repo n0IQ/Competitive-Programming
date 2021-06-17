e3cf535e6eb31619b3cc2277b6bbe5644d4c1fab
Mohit Varma <varmamohit122@gmail.com>
2021-06-17 20:17:00 +0530

    Add solution for problem `A - Remove Duplicates` Link: https://codeforces.com/contest/978/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/978/submission/119722803

---
+++ codeforces/978/A.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vi a(n), b;
+	set<int> s;
+
+	rep(i, 0, n) cin >> a[i];
+
+	repR(i, n - 1, 0) {
+		if (s.count(a[i]) == 0) {
+			s.insert(a[i]);
+			b.pb(a[i]);
+		}
+	}
+
+	cout << sz(b) << '\n';
+	repR(i, sz(b) - 1, 0) cout << b[i] << " ";
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	// /cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
