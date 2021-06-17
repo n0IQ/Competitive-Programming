f7e10f11000a7370fa6fd8c5e8fa4b9e1b92dc2e
Mohit Varma <varmamohit122@gmail.com>
2021-06-18 03:00:00 +0530

    Add solution for problem `C - Two Teams Composing` Link: https://codeforces.com/contest/1335/problem/C Tags: binary search, greedy, implementation, sortings, *1100 Ref: https://codeforces.com/contest/1335/submission/119747802

---
+++ codeforces/1335/C.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	map<int, int> cnt;
+	rep(i, 0, n) {
+		int x; cin >> x;
+		cnt[x]++;
+	}
+
+	if (n == 1) {
+		cout << 0 << '\n';
+		return;
+	}
+
+	if (sz(cnt) == n) {
+		cout << 1 << '\n';
+		return;
+	}
+
+	int sz1 = sz(cnt) - 1, sz2 = 0;
+	for (auto &it : cnt) sz2 = max(sz2, it.ss);
+
+	int res = min(sz1, sz2);
+
+	if (sz2 - sz1 >= 2) res++;
+
+	cout << res << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
