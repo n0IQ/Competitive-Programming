0e8a023888e2125fddc86d7729d525be2488945d
Mohit Varma <varmamohit122@gmail.com>
2021-07-07 19:12:00 +0530

    Add solution for problem `A - Unusual Competitions` Link: https://codeforces.com/contest/1322/problem/A Tags: greedy, *1300 Ref: https://codeforces.com/contest/1322/submission/121568079

---
+++ codeforces/1322/A.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	string s;
+	cin >> n >> s;
+
+	int open = 0, close = 0;
+	rep(i, 0, n) {
+		if (s[i] == '(') open++;
+		else close++;
+	}
+
+	if (open != close) {
+		cout << -1 << '\n';
+		return;
+	}
+
+	int p[n] = {0}, t = 0;
+	rep(i, 0, n) {
+		if (s[i] == '(') p[i] = ++t;
+		else p[i] = --t;
+	}
+
+	int ans = 0, l = 0;
+	rep(i, 1, n) {
+		if (p[i] == 0 && p[i - 1] == -1) {
+			ans += (i - l) + 1;
+		}
+
+		if (p[i] == 0) l = i + 1;
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
