b50f358e830c7ba5d62bfce5f0ce2a75faaaddb7
Mohit Varma <varmamohit122@gmail.com>
2021-07-18 01:55:00 +0530

    Add solution for problem `B - Putting Plates` Link: https://codeforces.com/contest/1530/problem/B Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1530/submission/122868445

---
+++ codeforces/1530/B.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int h, w;
+	cin >> h >> w;
+
+	int a[h][w];
+	mem0(a);
+
+	for (int j = 0; j < w; j += 2) {
+		a[0][j] = 1;
+		a[h - 1][j] = 1;
+	}
+
+	for (int i = 2; i < h - 2; i += 2) {
+		a[i][0] = 1;
+		a[i][w - 1] = 1;
+	}
+
+	rep(i, 0, h) {
+		rep(j, 0, w) {
+			cout << a[i][j];
+		}
+
+		cout << '\n';
+	}
+
+	cout << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
