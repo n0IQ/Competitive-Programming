07883e6e2864c8f41bac18f730db4f84165467e6
Mohit Varma <varmamohit122@gmail.com>
2021-07-20 18:04:00 +0530

    Add solution for problem `C - String Equality` Link: https://codeforces.com/contest/1451/problem/C Tags: dp, greedy, hashing, implementation, strings, *1400 Ref: https://codeforces.com/contest/1451/submission/123105253

---
+++ codeforces/1451/C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, k;
+	string a, b;
+	cin >> n >> k >> a >> b;
+
+	int cnt1[27] = {0}, cnt2[27] = {0};
+
+	for (auto c : a) cnt1[c - 'a']++;
+	for (auto c : b) cnt2[c - 'a']++;
+
+	rep(i, 0, 26) {
+		if (cnt1[i] < cnt2[i] || (cnt1[i] -= cnt2[i]) % k) {
+			cout << "No" << '\n';
+			return;
+		}
+
+		cnt1[i + 1] += cnt1[i];
+	}
+
+	cout << "Yes" << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
