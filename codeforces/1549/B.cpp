9a169dc60c6fe4ab1cdf6ffffce6e5b19fa0565e
Mohit Varma <varmamohit122@gmail.com>
2021-08-01 20:32:00 +0530

    Add solution for problem `B - Gregor and the Pawn Game` Link: https://codeforces.com/contest/1549/problem/B Tags: dfs and similar, dp, flows, graph matchings, graphs, greedy, implementation, *800 Ref: https://codeforces.com/contest/1549/submission/124546445

---
+++ codeforces/1549/B.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	string s, t;
+	cin >> n >> s >> t;
+
+	int mat[2][n];
+	mem0(mat);
+
+	rep(i, 0, n) {
+		if (s[i] == '1') mat[0][i] = 1;
+	}
+
+	rep(i, 0, n) {
+		if (t[i] == '1') mat[1][i] = 1;
+	}
+
+	int i = 1, ans = 0;
+	rep(j, 0, n) {
+		if (mat[i][j] == 0) continue;
+		else if (mat[i - 1][j] == 0) {
+			ans++;
+			mat[i - 1][j] = -1;
+		}
+		else if (j == 0) {
+			if (mat[i - 1][j + 1] == 1) {
+				ans++;
+				mat[i - 1][j + 1] = -1;
+			}
+		}
+		else if (j == n - 1) {
+			if (mat[i - 1][j - 1] == 1) {
+				ans++;
+				mat[i - 1][j - 1] = -1;
+			}
+		}
+		else {
+			if (mat[i - 1][j - 1] == 1) {
+				ans++;
+				mat[i - 1][j - 1] = -1;
+			}
+			else if (mat[i - 1][j + 1] == 1) {
+				ans++;
+				mat[i - 1][j + 1] = -1;
+			}
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
