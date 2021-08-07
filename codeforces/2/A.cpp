fc1de1f2f62dfdbcad5073375a5d787a6f0a1116
Mohit Varma <varmamohit122@gmail.com>
2021-08-07 22:51:00 +0530

    Add solution for problem `A - Winner` Link: https://codeforces.com/contest/2/problem/A Tags: hashing, implementation, *1500 Ref: https://codeforces.com/contest/2/submission/125159100

---
+++ codeforces/2/A.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	map<string, int> scores1, scores2;
+	vector<pair<string, int>> game;
+
+	rep(i, 0, n) {
+		string name;
+		int pts;
+		cin >> name >> pts;
+		scores1[name] += pts;
+		game.pb({name, pts});
+	}
+
+	int mx = -1e9;
+
+	for (auto it : scores1) mx = max(mx, it.ss);
+
+	rep(i, 0, n) {
+		string name = game[i].ff;
+		int pts = game[i].ss;
+
+		scores2[name] += pts;
+
+		if (scores1[name] == mx && scores2[name] >= mx) {
+			cout << name << '\n';
+			return;
+		}
+	}
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
