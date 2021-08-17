765dc885b7d9fee783f7651773fd952590898939
Mohit Varma <varmamohit122@gmail.com>
2021-08-17 19:00:00 +0530

    Add solution for problem `D - Binary String Minimizing` Link: https://codeforces.com/contest/1256/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/1256/submission/126182629

---
+++ codeforces/1256/D.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n;
+	ll k;
+	string s;
+	cin >> n >> k >> s;
+
+	set<int> add, remove;
+	int j = 0;
+
+	rep(i, 0, n) {
+		if (s[i] == '0') {
+			if (k >= i - j) {
+				k -= (i - j);
+				remove.insert(i);
+				add.insert(j);
+				j++;
+			}
+			else {
+				remove.insert(i);
+				add.insert(i - k);
+				break;
+			}
+		}
+	}
+
+	for (auto it : remove) s[it] = '1';
+	for (auto it : add) s[it] = '0';
+	cout << s << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
