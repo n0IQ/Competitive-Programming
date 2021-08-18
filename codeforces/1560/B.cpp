89a94357addc191240be3642c49ab65b74b51ede
Mohit Varma <varmamohit122@gmail.com>
2021-08-18 20:52:00 +0530

    Add solution for problem `B - Who's Opposite?` Link: https://codeforces.com/contest/1560/problem/B Tags: math, *800 Ref: https://codeforces.com/contest/1560/submission/126320156

---
+++ codeforces/1560/B.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	ll a, b, c;
+	cin >> a >> b >> c;
+
+	if (a > b) swap(a, b);
+	ll d = b - a;
+	ll mx = d * 2;
+
+	if (c > mx || a > mx || b > mx || (c - d < 1 && c + d > mx)) cout << -1 << '\n';
+	else {
+		if (c + d <= mx) cout << c + d << '\n';
+		else cout << c - d << '\n';
+	}
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
