fc1cf5ab59e1f714a5dfdd0adc17bd503fa96fd1
Mohit Varma <varmamohit122@gmail.com>
2021-08-28 22:59:00 +0530

    Add solution for problem `B - Codeforces Subsequences` Link: https://codeforces.com/contest/1368/problem/B Tags: brute force, constructive algorithms, greedy, math, strings, *1500 Ref: https://codeforces.com/contest/1368/submission/127280440

---
+++ codeforces/1368/B.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	ll n;
+	cin >> n;
+
+	string s = "codeforces";
+	vector<int> a(10, 1);
+	int j = 0;
+
+	while (1) {
+		ll t = 1;
+		rep(i, 0, 10) t *= a[i];
+		if (t >= n) break;
+		a[j % 10]++;
+		j++;
+	}
+
+	string ans;
+	rep(i, 0, 10) {
+		string temp(a[i], s[i]);
+		ans += temp;
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
