270a71113c625d4bb9318c4cb883b08ac42ad42a
Mohit Varma <varmamohit122@gmail.com>
2021-08-31 16:06:00 +0530

    Add solution for problem `C - Tiles` Link: https://codeforces.com/contest/1178/problem/C Tags: combinatorics, greedy, math, *1300 Ref: https://codeforces.com/contest/1178/submission/127515112

---
+++ codeforces/1178/C.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 998244353
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+int mult(int a, int b)
+{
+	int res = (a * 1LL * b) % mod;
+
+	if (res < 0) {
+		res += mod;
+	}
+
+	return res;
+}
+
+int power(int a, int b)
+{
+	int res = 1;
+
+	while (b) {
+		if (b % 2 == 1) {
+			res = mult(a, res);
+		}
+
+		a = mult(a, a);
+		b /= 2;
+	}
+
+	return res;
+}
+
+void solve()
+{
+	int w, h;
+	cin >> w >> h;
+
+	cout << power(2, w + h) << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
