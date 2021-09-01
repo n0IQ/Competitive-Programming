838496e3f3d0849c09dfe03a9b9c55b3adc1ec51
Mohit Varma <varmamohit122@gmail.com>
2021-09-02 02:42:00 +0530

    Add solution for problem `B - Queries on a String` Link: https://codeforces.com/contest/598/problem/B Tags: implementation, strings, *1300 Ref: https://codeforces.com/contest/598/submission/127648330

---
+++ codeforces/598/B.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	string s;
+	int q;
+	cin >> s >> q;
+
+	while (q--) {
+		int l, r, k;
+		cin >> l >> r >> k;
+		--l, --r;
+		string t = s.substr(l, r - l + 1);
+
+		int m = k % sz(t);
+		string temp;
+		rep(i, sz(t) - m, sz(t)) temp.pb(t[i]);
+		rep(i, 0, sz(t) - m) temp.pb(t[i]);
+
+		for (int i = l, j = 0; i <= r; i++, j++) s[i] = temp[j];
+	}
+
+	cout << s << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin>>t;
+	while (t--) solve();
+
+	return 0;
+}
