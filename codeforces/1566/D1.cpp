16363670b674d6627de875b86f43cc5d2e4d8c4b
Mohit Varma <varmamohit122@gmail.com>
2021-09-12 21:28:00 +0530

    Add solution for problem `D1 - Seating Arrangements (easy version) ` Link: https://codeforces.com/contest/1566/problem/D1 Tags: data structures, greedy, sortings, *1100 Ref: https://codeforces.com/contest/1566/submission/128643424

---
+++ codeforces/1566/D1.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> a(m);
+	rep(i, 0, m) cin >> a[i];
+
+	vector<int> b(a), mark(m, false);
+	sort(all(b));
+
+	ll ans = 0LL;
+	rep(i, 0, m) {
+		int num = a[i], x;
+
+		repR(j, m - 1, 0) {
+			if (b[j] == num && mark[j] == false) {
+				mark[j] = true;
+				x = j;
+				break;
+			}
+		}
+
+		rep(j, 0, x) {
+			if (mark[j]) ans++;
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
