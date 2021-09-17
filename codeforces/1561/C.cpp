dc3b41ae8a1d22ebc32640582f05d405b6ab9a03
Mohit Varma <varmamohit122@gmail.com>
2021-09-17 17:05:00 +0530

    Add solution for problem `C - Deep Down Below` Link: https://codeforces.com/contest/1561/problem/C Tags: binary search, greedy, sortings, *1300 Ref: https://codeforces.com/contest/1561/submission/129060271

--- codeforces/1561/C.cpp
+++ codeforces/1561/C.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
 using namespace std;
 
 #define pb push_back
@@ -36,7 +36,7 @@ typedef pair<ll, ll> pll;
 #define mod 1000000007
 #define ps(x,y) fixed << setprecision(y) << x
 #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
-template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 int n;
 vector<vector<ll>> adj;
