0013e47b5b850db5c1b75a7b0f3cfeb5f19d5711
Mohit Varma <varmamohit122@gmail.com>
2021-09-26 17:41:00 +0530

    Add solution for problem `B - Base K` Link: https://atcoder.jp/contests/abc220/tasks/abc220_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc220/submissions/26132775

---
+++ atcoder/abc220/B.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll val(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (ll)c - '0';
+	else
+		return (ll)c - 'A' + 10;
+}
+
+int toDeci(string s, ll base)
+{
+	ll len = sz(s);
+	ll power = 1;
+	ll num = 0;
+	ll i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (val(s[i]) >= base)
+		{
+			return -1;
+		}
+
+		num += val(s[i]) * power;
+		power = power * base;
+	}
+
+	return num;
+}
+
+void solve()
+{
+	ll k, a, b;
+	cin >> k >> a >> b;
+
+	a = toDeci(to_string(a), k);
+	b = toDeci(to_string(b), k);
+	cout << a * b << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
