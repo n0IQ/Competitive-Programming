33de790de27047f9f58f6ebdc00f8207cb11ec9f
Mohit Varma <varmamohit122@gmail.com>
2021-10-02 21:10:00 +0530

    Add solution for problem `D - Online games` Link: https://atcoder.jp/contests/abc221/tasks/abc221_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc221/submissions/26322258

---
+++ atcoder/abc221/D.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for (int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define mod 1000000007
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vector<int> ans(n + 1);
+	vector<pii> a;
+
+	rep(i, 0, n) {
+		int x, y; cin >> x >> y;
+		a.pb({x, 1});
+		a.pb({x + y, -1});
+	}
+
+	sort(all(a));
+	int cnt = 0, prv = 0;
+
+	for (auto &[x, y] : a) {
+		ans[cnt] += x - prv;
+		cnt += y;
+		prv = x;
+	}
+
+	rep(i, 1, n + 1) cout << ans[i] << ' ';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+	     freopen("input.txt","r",stdin);
+	     freopen("output.txt","w",stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
