d9b0bd383a9c0d4dd999a254ee75f5f906d0b25d
Mohit Varma <varmamohit122@gmail.com>
2021-10-13 22:02:00 +0530

    Add solution for problem `B - Make it Divisible by 25` Link: https://codeforces.com/contest/1593/problem/B Tags: dfs and similar, dp, greedy, math, math, *900 Ref: https://codeforces.com/contest/1593/submission/131842340

---
+++ codeforces/1593/B.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+void solve()
+{
+	ll n;
+	cin >> n;
+
+	string s = to_string(n);
+
+	int ans = 1e9;
+	repR(i, sz(s) - 1, 0) {
+		if (s[i] == '0') {
+			repR(j, i - 1, 0) {
+				if (s[j] == '0' || s[j] == '5') {
+					ans = min(ans, sz(s) - j - 2);
+				}
+			}
+		}
+		else if (s[i] == '5') {
+			repR(j, i - 1, 0) {
+				if (s[j] == '2' || s[j] == '7') {
+					ans = min(ans, sz(s) - j - 2);
+				}
+			}
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	//freopen("input.txt","r",stdin);
+	//freopen("output.txt","w",stdout);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
