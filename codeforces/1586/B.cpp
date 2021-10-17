807861cd67c17002fcbfb81f7d4146524131e08b
Mohit Varma <varmamohit122@gmail.com>
2021-10-17 17:52:00 +0530

    Add solution for problem `B - Omkar and Heavenly Tree` Link: https://codeforces.com/contest/1586/problem/B Tags: brute force, constructive algorithms, trees, *1200 Ref: https://codeforces.com/contest/1586/submission/132244311

---
+++ codeforces/1586/B.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+
+	bool a[n] = {0};
+	rep(i, 0, m) {
+		int u, x, v;
+		cin >> u >> x >> v;
+		--u, --x, --v;
+		a[x] = 1;
+	}
+
+	int root = 0;
+	rep(i, 0, n) {
+		if (a[i] == 0) {
+			root = i;
+			break;
+		}
+	}
+
+	rep(i, 0, n) {
+		if (i == root) continue;
+		cout << root + 1 << ' ' << i + 1 << '\n';
+	}
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
