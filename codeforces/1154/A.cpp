e89b4629e63b92524d07d3305e5b79bb7299e0ff
Mohit Varma <varmamohit122@gmail.com>
2021-11-02 15:32:00 +0530

    Add solution for problem `A - Restoring Three Numbers` Link: https://codeforces.com/contest/1154/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1154/submission/134037519

---
+++ codeforces/1154/A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+void solve()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	int mx = max({a, b, c, d});
+
+	if (a == mx) cout << a - b << ' ' << a - c << ' ' << a - d << '\n';
+	else if (b == mx) cout << b - a << ' ' << b - c << ' ' << b - d << '\n';
+	else if (c == mx) cout << c - a << ' ' << c - b << ' ' << c - d << '\n';
+	else if (d == mx) cout << d - a << ' ' << d - b << ' ' << d - c << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
