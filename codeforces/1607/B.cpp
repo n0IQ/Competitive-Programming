241da1aad392a29c66e2cf93f969a5e63ce6cbf7
Mohit Varma <varmamohit122@gmail.com>
2021-11-02 20:25:00 +0530

    Add solution for problem `B - Odd Grasshopper` Link: https://codeforces.com/contest/1607/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1607/submission/134078017

---
+++ codeforces/1607/B.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+void solve()
+{
+	ll x, n;
+	cin >> x >> n;
+
+	ll t = n / 4;
+	ll rem = n - (t * 4);
+
+	ll step = (t * 4) + 1;
+
+	while (rem--) {
+		if (abs(x) % 2 == 0) x -= step;
+		else x += step;
+
+		step++;
+	}
+
+	cout << x << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
