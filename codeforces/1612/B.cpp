ad4410bc738b39df1311d29911aeb12d7ae08365
Mohit Varma <varmamohit122@gmail.com>
2021-11-22 19:06:00 +0530

    Add solution for problem `B - Special Permutation` Link: https://codeforces.com/contest/1612/problem/B Tags: constructive algorithms, greedy, *900 Ref: https://codeforces.com/contest/1612/submission/136521162

---
+++ codeforces/1612/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+void solve()
+{
+	int n, a, b;
+	cin >> n >> a >> b;
+
+	vector<int> arr(n), extra;
+	arr[0] = a, arr[n - 1] = b;
+	rep(i, 1, n + 1) {
+		if (i != a && i != b) extra.pb(i);
+	}
+
+	reverse(all(extra));
+	rep(i, 1, n - 1) {
+		arr[i] = extra[i - 1];
+	}
+
+	if ((*min_element(arr.begin(), arr.begin() + n / 2) == a) && (*max_element(arr.begin() + n / 2, arr.end()) == b)) {
+		for (auto x : arr) cout << x << ' ';
+		cout << '\n';
+	}
+	else {
+		cout << -1 << '\n';
+	}
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif*/
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
