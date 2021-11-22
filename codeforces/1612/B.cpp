cc3a3fa513a47ed1fe5a7b84769632e3cbd91434
Mohit Varma <varmamohit122@gmail.com>
2021-11-22 16:18:00 +0530

    Add solution for problem `B - Special Permutation` Link: https://codeforces.com/contest/1612/problem/B Tags: constructive algorithms, greedy, *900 Ref: https://codeforces.com/contest/1612/submission/136454430

--- codeforces/1612/B.cpp
+++ codeforces/1612/B.cpp
@@ -38,24 +38,54 @@ void solve()
 	int n, a, b;
 	cin >> n >> a >> b;
 
-	vector<int> arr(n), extra;
-	arr[0] = a, arr[n - 1] = b;
-	rep(i, 1, n + 1) {
-		if (i != a && i != b) extra.pb(i);
-	}
-
-	reverse(all(extra));
-	rep(i, 1, n - 1) {
-		arr[i] = extra[i - 1];
-	}
-
-	if ((*min_element(arr.begin(), arr.begin() + n / 2) == a) && (*max_element(arr.begin() + n / 2, arr.end()) == b)) {
-		for (auto x : arr) cout << x << ' ';
-		cout << '\n';
-	}
-	else {
+	int m = n / 2;
+	if ((a <= m && b <= m) || (a > m && b > m) || (a > m + 1) || (b < m)) {
 		cout << -1 << '\n';
+		return;
 	}
+
+	int arr[n];
+	vector<int> v;
+	rep(i, 1, n + 1) {
+		if (i != a && i != b) v.pb(i);
+	}
+
+	bool mark[n + 1] = {0};
+	arr[0] = a, arr[n / 2] = b;
+	int j = 1, k = (n / 2) + 1;
+
+	rep(i, 0, sz(v)) {
+		if (v[i] < a) {
+			arr[k] = v[i];
+			k++;
+			mark[v[i]] = 1;
+		}
+		else if (v[i] > b) {
+			arr[j] = v[i];
+			j++;
+			mark[v[i]] = 1;
+		}
+	}
+
+	set<int> s;
+	rep(i, 0, sz(v)) {
+		if (mark[v[i]] == 0) s.insert(v[i]);
+	}
+
+	while (j < n / 2) {
+		arr[j] = *s.begin();
+		s.erase(s.begin());
+		j++;
+	}
+
+	while (k < n) {
+		arr[k] = *s.begin();
+		s.erase(s.begin());
+		k++;
+	}
+
+	rep(i, 0, n) cout << arr[i] << ' ';
+	cout << '\n';
 }
 
 int main()
