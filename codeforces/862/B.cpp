c101a7582013c522862d09a667de9fdd7b21ed99
Mohit Varma <varmamohit122@gmail.com>
2021-12-03 16:53:00 +0530

    Add solution for problem `B - Mahmoud and Ehab and the bipartiteness` Link: https://codeforces.com/contest/862/problem/B Tags: dfs and similar, graphs, trees, *1300 Ref: https://codeforces.com/contest/862/submission/137876946

---
+++ codeforces/862/B.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem1(a) memset(a,-1,sizeof(a))
+#define mem0(a) memset(a,0,sizeof(a))
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+const int MAXN = (int)1e5 + 10;
+vector<int> adj[MAXN], lv[MAXN];
+ll cnt[2];
+
+void dfs(int u, int p = -1, int l = 0)
+{
+	lv[l].pb(u);
+	for (auto v : adj[u]) {
+		if (v != p) {
+			dfs(v, u, l + 1);
+		}
+	}
+}
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	rep(i, 0, n - 1) {
+		int u, v;
+		cin >> u >> v;
+		--u, --v;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+
+	dfs(0);
+	ll ans = 0;
+	rep(i, 1, MAXN) {
+		ll t = sz(lv[i]) * 1LL * (sz(lv[i - 1]) - 1);
+		ans += t + (sz(lv[i]) * 1LL * cnt[i % 2]);
+		cnt[i % 2] += sz(lv[i - 1]);
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+	fastio;
+
+	/*#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif*/
+
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
