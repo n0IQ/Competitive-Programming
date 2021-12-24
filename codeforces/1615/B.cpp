8a9e96e038ceb475224da1a2444059df7f52b031
Mohit Varma <varmamohit122@gmail.com>
2021-12-24 21:49:00 +0530

    Add solution for problem `B - And It's Non-Zero` Link: https://codeforces.com/contest/1615/problem/B Tags: bitmasks, greedy, math, *1300 Ref: https://codeforces.com/contest/1615/submission/140493429

---
+++ codeforces/1615/B.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem0(a) memset(a,0,sizeof(a))
+#define mem1(a) memset(a,-1,sizeof(a))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+/*---------------------------------------------------------Debugging----------------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(ll t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+const int MAXN = (int)2e5 + 10;
+vector<int> a[40];
+
+void precalc()
+{
+	for (int i = 1; i < MAXN ; i++) {
+		int x = i, pos = 0;
+
+		while (x) {
+			if ((x & 1)) {
+				a[pos].pb(i);
+			}
+
+			x >>= 1;
+			pos++;
+		}
+	}
+
+	rep(i, 0, 40) {
+		sort(all(a[i]));
+	}
+}
+
+void solve()
+{
+	int l, r;
+	cin >> l >> r;
+
+	int mx = 0;
+	rep(i, 0, 40) {
+		auto it1 = lb(all(a[i]), l);
+		if (it1 == a[i].end() || *it1 > r) continue;
+		auto it2 = ub(all(a[i]), r);
+		it2--;
+		if (*it2 < l) continue;
+		int j = it2 - it1;
+		debug(j)
+		mx = max(mx, j + 1);
+	}
+
+	cout << (r - l + 1) - mx << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+
+	fastio;
+	precalc();
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
