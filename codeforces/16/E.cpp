d982cde40cdbdbbf5ac0b674dd8e9b501e34cc90
Mohit Varma <varmamohit122@gmail.com>
2022-01-01 19:26:00 +0530

    Add solution for problem `E - Fish` Link: https://codeforces.com/contest/16/problem/E Tags: bitmasks, dp, probabilities, *1900 Ref: https://codeforces.com/contest/16/submission/141320130

---
+++ codeforces/16/E.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+#define set_bits __builtin_popcountll
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem0(a) memset(a,0,sizeof(a))
+#define mem1(a) memset(a,-1,sizeof(a))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+/*---------------------------------------------------------Debugging----------------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(ll t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	lld arr[n][n];
+	rep(i, 0, n) {
+		rep(j, 0, n) {
+			cin >> arr[i][j];
+		}
+	}
+
+	vector<lld> dp(1 << 19);
+	dp[(1 << n) - 1] = 1.0; // initially all fish are alive set their probability to 1
+
+	repR(mask, (1 << n) - 1, 0) {
+		rep(i, 0, n) {
+			if (mask & (1 << i)) { // ith fish is alive
+				rep(j, 0, n) {
+					if (mask & (1 << j) && i != j) { // jth fish is alive
+						ll p = set_bits(mask);
+						dp[mask ^ (1 << j)] += dp[mask] * arr[i][j] / ((p * (p - 1)) / 2);
+					}
+				}
+			}
+		}
+	}
+
+	rep(i, 0, n) {
+		cout << ps(dp[(1 << i)], 6) << ' ';
+	}
+
+	cout << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+
+	fastio;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
