d85c7b77437d9589ede2e7c224fe1ad0edbc3f32
Mohit Varma <varmamohit122@gmail.com>
2022-01-25 22:05:00 +0530

    Add solution for problem `D - Powerful array` Link: https://codeforces.com/contest/86/problem/D Tags: data structures, implementation, math, two pointers, *2200 Ref: https://codeforces.com/contest/86/submission/143997524

--- codeforces/86/D.cpp
+++ codeforces/86/D.cpp
@@ -66,45 +66,20 @@ template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i
 const int MAXN = (int)2e5 + 10;
 int BLOCK_SIZE;
 
-inline int64_t hilbertOrder(int x, int y, int pow, int rotate) {
-	if (pow == 0) {
-		return 0;
-	}
-	int hpow = 1 << (pow - 1);
-	int seg = (x < hpow) ? (
-	              (y < hpow) ? 0 : 3
-	          ) : (
-	              (y < hpow) ? 1 : 2
-	          );
-	seg = (seg + rotate) & 3;
-	const int rotateDelta[4] = {3, 0, 0, 1};
-	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
-	int nrot = (rotate + rotateDelta[seg]) & 3;
-	int64_t subSquareSize = int64_t(1) << (2 * pow - 2);
-	int64_t ans = seg * subSquareSize;
-	int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
-	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-	return ans;
-}
-
 struct Query {
 	int l, r, idx;
-	int64_t ord;
-
-	inline void calcOrder() {
-		ord = hilbertOrder(l, r, 21, 0);
-	}
-};
-
-inline bool operator<(const Query &a, const Query &b) {
-	return a.ord < b.ord;
-}
+} qr[MAXN];
 
 int n, q;
-Query qr[MAXN];
 int arr[MAXN], freq[(int)1e6 + 10];
 ll ans[MAXN];
 ll curr_ans;
+
+bool comp(const Query &a, const Query &b)
+{
+	if (a.l / BLOCK_SIZE != b.l / BLOCK_SIZE) return a.l < b.l;
+	else return (a.l / BLOCK_SIZE & 1) ? a.r < b.r: a.r > b.r;
+}
 
 inline void add(int x)
 {
@@ -123,7 +98,7 @@ inline void remove(int x)
 void mo()
 {
 	BLOCK_SIZE = sqrt(n) + 1;
-	sort(qr + 1, qr + q + 1);
+	sort(qr + 1, qr + q + 1, comp);
 	int l = 1, r = 0;
 	curr_ans = 0;
 
@@ -146,7 +121,6 @@ void solve()
 		int l, r;
 		cin >> l >> r;
 		qr[i] = {l, r, i};
-		qr[i].calcOrder();
 	}
 
 	mo();
