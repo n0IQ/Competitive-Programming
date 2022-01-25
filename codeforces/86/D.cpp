ad251f60d9c1d394c9284d32c14be4285cbcf3f6
Mohit Varma <varmamohit122@gmail.com>
2022-01-26 00:21:00 +0530

    Add solution for problem `D - Powerful array` Link: https://codeforces.com/contest/86/problem/D Tags: data structures, implementation, math, two pointers, *2200 Ref: https://codeforces.com/contest/86/submission/144009608

---
+++ codeforces/86/D.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem0(a) memset(a,0,sizeof(a))
+#define mem1(a) memset(a,-1,sizeof(a))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+/*---------------------------------------------------------Debugging----------------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(ll t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+const int MAXN = (int)2e5 + 10;
+int BLOCK_SIZE;
+
+inline int64_t hilbertOrder(int x, int y, int pow, int rotate) {
+	if (pow == 0) {
+		return 0;
+	}
+	int hpow = 1 << (pow - 1);
+	int seg = (x < hpow) ? (
+	              (y < hpow) ? 0 : 3
+	          ) : (
+	              (y < hpow) ? 1 : 2
+	          );
+	seg = (seg + rotate) & 3;
+	const int rotateDelta[4] = {3, 0, 0, 1};
+	int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
+	int nrot = (rotate + rotateDelta[seg]) & 3;
+	int64_t subSquareSize = int64_t(1) << (2 * pow - 2);
+	int64_t ans = seg * subSquareSize;
+	int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
+	ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
+	return ans;
+}
+
+struct Query {
+	int l, r, idx;
+	int64_t ord;
+
+	inline void calcOrder() {
+		ord = hilbertOrder(l, r, 21, 0);
+	}
+};
+
+inline bool operator<(const Query &a, const Query &b) {
+	return a.ord < b.ord;
+}
+
+int n, q;
+Query qr[MAXN];
+int arr[MAXN], freq[(int)1e6 + 10];
+ll ans[MAXN];
+ll curr_ans;
+
+inline void add(int x)
+{
+	curr_ans -= 1LL * freq[x] * freq[x] * x;
+	freq[x]++;
+	curr_ans += 1LL * freq[x] * freq[x] * x;
+}
+
+inline void remove(int x)
+{
+	curr_ans -= 1LL * freq[x] * freq[x] * x;
+	freq[x]--;
+	curr_ans += 1LL * freq[x] * freq[x] * x;
+}
+
+void mo()
+{
+	BLOCK_SIZE = sqrt(n) + 1;
+	sort(qr + 1, qr + q + 1);
+	int l = 1, r = 0;
+	curr_ans = 0;
+
+	rep(i, 1, q + 1) {
+		while (r < qr[i].r) add(arr[++r]);
+		while (l > qr[i].l) add(arr[--l]);
+		while (r > qr[i].r) remove(arr[r--]);
+		while (l < qr[i].l) remove(arr[l++]);
+		ans[qr[i].idx] = curr_ans;
+	}
+}
+
+void solve()
+{
+	cin >> n >> q;
+
+	rep(i, 1, n + 1) cin >> arr[i];
+
+	rep(i, 1, q + 1) {
+		int l, r;
+		cin >> l >> r;
+		qr[i] = {l, r, i};
+		qr[i].calcOrder();
+	}
+
+	mo();
+
+	rep(i, 1, q + 1) {
+		cout << ans[i] << '\n';
+	}
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+
+	fastio;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
