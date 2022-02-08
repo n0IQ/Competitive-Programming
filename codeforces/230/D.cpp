c01f4cfe1aefcc7793ad2dba93d645efe77637d7
Mohit Varma <varmamohit122@gmail.com>
2022-02-09 03:27:00 +0530

    Add solution for problem `D - Planets` Link: https://codeforces.com/contest/230/problem/D Tags: binary search, graphs, shortest paths, *1700 Ref: https://codeforces.com/contest/230/submission/145694960

---
+++ codeforces/230/D.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define ff first
+#define ss second
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repR(i, a, b) for(int i = a; i >= b; --i)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define mem0(a) memset(a,0,sizeof(a))
+#define mem1(a) memset(a,-1,sizeof(a))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define MOD1 1000000007
+#define MOD2 998244353
+#define INF  2000000000000000000
+#define ps(x,y) fixed << setprecision(y) << x
+#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+/*---------------------------------------------------------Debugging----------------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int t) {cerr << t;}
+void _print(ll t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
+vector<pll> adj[(int)1e5 + 10];
+vector<int> arrival[(int)1e5 + 10];
+vector<ll> dist;
+
+void Dijkstra(int n)
+{
+	priority_queue<pll, vector<pll>, greater<pll>> pq;
+	dist.assign(n + 1, INF);
+	dist[1] = 0;
+	pq.push({0, 1});
+
+	while (!pq.empty()) {
+		int u = pq.top().ss;
+		ll d = pq.top().ff;
+		pq.pop();
+
+		if (d != dist[u]) {
+			continue;
+		}
+		if (u == n) break;
+
+		auto it = lb(all(arrival[u]), d);
+		if (it != arrival[u].end() && *it == d) {
+			d++;
+			it++;
+			while (it != arrival[u].end() && *it == d) {
+				d++;
+				it++;
+			}
+		}
+
+		for (auto e : adj[u]) {
+			ll v = e.ff, w = e.ss;
+
+			if (d + w < dist[v]) {
+				dist[v] = d + w;
+				pq.push({dist[v], v});
+			}
+		}
+	}
+}
+
+void solve()
+{
+	int n, m;
+	cin >> n >> m;
+
+	rep(i, 0, m) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].pb({v, w});
+		adj[v].pb({u, w});
+	}
+
+	rep(i, 1, n + 1) {
+		int k;
+		cin >> k;
+
+		rep(j, 0, k) {
+			int t;
+			cin >> t;
+			arrival[i].pb(t);
+		}
+	}
+
+	Dijkstra(n);
+
+	if (dist[n] == INF) cout << "-1\n";
+	else cout << dist[n] << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+
+	fastio;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
