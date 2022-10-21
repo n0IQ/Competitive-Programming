b9b77c3b7fd4585d62a4a70f76c67f6e6010d816
Mohit <varmamohit122@gmail.com>
2022-10-21 20:08:00 +0530

    Add solution for problem `A - Maxmina` Link: https://codeforces.com/contest/1746/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1746/submission/177326181

---
+++ codeforces/1746/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> a(n);
+	int cnt = 0;
+	for (auto &x : a) {
+		cin >> x;
+		if (x == 1) cnt++;
+	}
+
+	if (cnt > 0) cout << "YES\n";
+	else cout << "NO\n";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
