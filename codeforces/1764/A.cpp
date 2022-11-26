e90307814f47ae2ef91ca7b5a1b7535a6a267381
Mohit Varma <varmamohit122@gmail.com>
2022-11-26 19:39:00 +0530

    Add solution for problem `A - Doremy's Paint` Link: https://codeforces.com/contest/1764/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1764/submission/182655633

---
+++ codeforces/1764/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	vector<int> a(n);
+	for (auto &x : a) cin >> x;
+
+	cout << 1 << ' ' << n << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
