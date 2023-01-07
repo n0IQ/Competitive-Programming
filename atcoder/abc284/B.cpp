a7ec8a4421fd819983485b5795c44cfa925fcf0e
Mohit Varma <varmamohit122@gmail.com>
2023-01-07 17:32:00 +0530

    Add solution for problem `B - Multi Test Cases` Link: https://atcoder.jp/contests/abc284/tasks/abc284_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc284/submissions/37795070

---
+++ atcoder/abc284/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int n;
+	cin >> n;
+
+	int odd = 0;
+	for (int i = 0; i < n; i++) {
+		ll x;
+		cin >> x;
+		if (x % 2) odd++;
+	}
+
+	cout << odd << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
