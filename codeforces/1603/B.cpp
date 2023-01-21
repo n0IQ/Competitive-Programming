36fe610d0fa1ecd328b4b4347b4d1ab5be774c26
Mohit Varma <varmamohit122@gmail.com>
2023-01-21 14:35:00 +0530

    Add solution for problem `B - Moderate Modular Mode` Link: https://codeforces.com/contest/1603/problem/B Tags: constructive algorithms, math, number theory, *1600 Ref: https://codeforces.com/contest/1603/submission/189941866

---
+++ codeforces/1603/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	ll x, y;
+	cin >> x >> y;
+
+	if (x == y) {
+		cout << x << '\n';
+	}
+	else if (x > y) {
+		cout << x + y << '\n';
+	}
+	else {
+		if (y % x == 0) cout << x << '\n';
+		else {
+			ll t = x * (y / x);
+			cout << (t + y) / 2 << '\n';
+		}
+	}
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
