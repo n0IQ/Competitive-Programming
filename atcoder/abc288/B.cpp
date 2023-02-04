b95b5d73b32b1e7c7251e8a01fbcf4c5bea028e3
Mohit Varma <varmamohit122@gmail.com>
2023-02-04 17:32:00 +0530

    Add solution for problem `B - Qualification Contest` Link: https://atcoder.jp/contests/abc288/tasks/abc288_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc288/submissions/38600003

---
+++ atcoder/abc288/B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+
+	multiset<string> st;
+	for (int i = 1; i <= n; i++) {
+		string s;
+		cin >> s;
+		if (i <= k)st.insert(s);
+	}
+
+	for (auto &s : st) {
+		cout << s << '\n';
+	}
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
