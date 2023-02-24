738bee9f542242cbc308c26baa017c5435d8d73b
Mohit Varma <varmamohit122@gmail.com>
2023-02-24 23:19:00 +0530

    Add solution for problem `C - Perfect Keyboard` Link: https://codeforces.com/contest/1303/problem/C Tags: dfs and similar, greedy, implementation, *1600 Ref: https://codeforces.com/contest/1303/submission/194820070

--- codeforces/1303/C.cpp
+++ codeforces/1303/C.cpp
@@ -7,42 +7,92 @@ void solve()
 	string s;
 	cin >> s;
 
-	int n = (int)s.size(), pos = 0;
+	int n = (int)s.size();
+
+	if (n == 1) {
+		cout << "YES\n";
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			cout << ch;
+		}
+		cout << '\n';
+		return;
+	}
+
+	vector<int> adj[26];
 	vector<bool> visited(26, 0);
 	visited[s[0] - 'a'] = 1;
-	string ans(1, s[0]);
 
-	for (int i = 1; i < n; i++) {
-		if (visited[s[i] - 'a']) {
-			if (pos > 0 && ans[pos - 1] == s[i]) {
-				pos--;
+	for (int i = 0; i < n - 1; i++) {
+		int u = s[i] - 'a', v = s[i + 1] - 'a';
+		if (visited[v]) {
+			bool found = false;
+			for (auto &x : adj[u]) {
+				if (x == v) found = true;
 			}
-			else if (pos < (int)ans.size() - 1 && ans[pos + 1] == s[i]) {
-				pos++;
-			}
-			else {
+
+			if (!found) {
 				cout << "NO\n";
 				return;
 			}
 		}
 		else {
-			if (pos == 0) {
-				ans = s[i] + ans;
-			}
-			else if (pos == (int)ans.size() - 1) {
-				ans.push_back(s[i]);
-				pos++;
-			}
-			else {
-				cout << "NO\n";
-				return;
-			}
+			adj[u].push_back(v);
+			adj[v].push_back(u);
 		}
-		visited[s[i] - 'a'] = 1;
+		visited[v] = 1;
 	}
 
 	for (int i = 0; i < 26; i++) {
-		if (!visited[i]) ans.push_back(char('a' + i));
+		if (adj[i].size() > 2) {
+			cout << "NO\n";
+			return;
+		}
+	}
+
+	fill(visited.begin(), visited.end(), 0);
+	string ans;
+
+	for (int i = 0; i < n; i++) {
+		int curr = s[i] - 'a';
+
+		if (visited[curr]) {
+			if (adj[curr].size() == 2) {
+				int idxCurr = ans.find('a' + curr);
+				int idxU = ans.find('a' + adj[curr][0]);
+				int idxV = ans.find('a' + adj[curr][1]);
+				/*cout << "idx: " << curr << ' ' << idxCurr << '\n';
+				cout << "idx: " << adj[curr][0] << ' ' << idxU << '\n';
+				cout << "idx: " << adj[curr][1] << ' ' << idxV << '\n';*/
+
+				if (idxU == -1) {
+					if (idxCurr == 0) ans.insert(ans.begin(), 'a' + adj[curr][0]);
+					else ans.push_back('a' + adj[curr][0]);
+					visited[adj[curr][0]] = 1;
+				}
+				if (idxV == -1) {
+					if (idxCurr == 0) ans.insert(ans.begin(), 'a' + adj[curr][1]);
+					else ans.push_back('a' + adj[curr][1]);
+					visited[adj[curr][1]] = 1;
+				}
+			}
+		}
+		else {
+			ans.push_back('a' + curr);
+			ans.push_back('a' + adj[curr][0]);
+			visited[curr] = 1;
+			visited[adj[curr][0]] = 1;
+
+			if (adj[curr].size() == 2) {
+				ans.insert(ans.begin(), ('a' + adj[curr][1]));
+				visited[adj[curr][1]] = 1;
+			}
+		}
+
+		// cout << ans << '\n';
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (!visited[i]) ans.push_back('a' + i);
 	}
 
 	cout << "YES\n";
