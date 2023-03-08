685c3bf3c935504b427f61a5cf7ded585010918b
Mohit Varma <varmamohit122@gmail.com>
2023-03-08 19:43:00 +0530

    Add solution for problem `A - Good ol' Numbers Coloring` Link: https://codeforces.com/contest/1245/problem/A Tags: math, number theory, *1000 Ref: https://codeforces.com/contest/1245/submission/196515547

---
+++ codeforces/1245/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int a, b;
+	cin >> a >> b;
+
+	if (__gcd(a, b) > 1) cout << "Infinite\n";
+	else cout << "Finite\n";
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
