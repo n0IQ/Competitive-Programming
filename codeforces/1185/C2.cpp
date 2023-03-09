1bf5b4d4ab91c93f091cab46b87c26fc01b208b0
Mohit Varma <varmamohit122@gmail.com>
2023-03-10 03:09:00 +0530

    Add solution for problem `C2 - Exam in BerSU (hard version)` Link: https://codeforces.com/contest/1185/problem/C2 Tags: brute force, data structures, greedy, math, *1700 Ref: https://codeforces.com/contest/1185/submission/196718373

---
+++ codeforces/1185/C2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	ll n, m;
+	cin >> n >> m;
+
+	vector<ll> cnt(101, 0);
+	for (int i = 1; i <= n; i++) {
+		ll x;
+		cin >> x;
+
+		ll reqSum = m - x, take = 1;
+
+		for (ll j = 1; j <= 100; j++) {
+			ll t = min(max(0LL, cnt[j]), reqSum / j);
+			reqSum -= (j * t);
+			take += t;
+		}
+
+		cout << i - take << ' ';
+		cnt[x]++;
+	}
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
