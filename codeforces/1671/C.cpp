0315f4f49654528b8f148d7e9d2549743514cce1
Mohit Varma <varmamohit122@gmail.com>
2023-03-11 13:46:00 +0530

    Add solution for problem `C - Dolce Vita` Link: https://codeforces.com/contest/1671/problem/C Tags: binary search, brute force, greedy, math, *1200 Ref: https://codeforces.com/contest/1671/submission/196903613

---
+++ codeforces/1671/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	int n;
+	ll x;
+	cin >> n >> x;
+
+	vector<ll> a(n);
+	for (auto &x : a) cin >> x;
+	sort(a.begin(), a.end());
+
+	ll ans = 0, prefixSum = 0;
+	for (ll i = 0; i < n; i++) {
+		if (a[i] > x) break;
+		ll l = 1, r = x + 10, sugar = 0;
+
+		while (l <= r) {
+			ll mid = l + (r - l) / 2;
+			ll itemPrice = a[i] + mid - 1;
+			ll totalPrice = prefixSum + (i * (mid - 1)) + itemPrice;
+
+			if (totalPrice <= x) {
+				sugar = mid;
+				l = mid + 1;
+			}
+			else {
+				r = mid - 1;
+			}
+		}
+
+		ans += sugar;
+		prefixSum += a[i];
+	}
+
+	cout << ans << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
