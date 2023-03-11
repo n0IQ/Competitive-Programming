69b1d365f04fc77e467d2a7a2f42165ebd45acfd
Mohit Varma <varmamohit122@gmail.com>
2023-03-11 17:32:00 +0530

    Add solution for problem `A - Swap Odd and Even` Link: https://atcoder.jp/contests/abc293/tasks/abc293_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc293/submissions/39601338

---
+++ atcoder/abc293/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve()
+{
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < (int)s.size(); i += 2) {
+		swap(s[i], s[i + 1]);
+	}
+
+	cout << s << '\n';
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//freopen("Error.txt", "w", stderr);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+
+	return 0;
+}
