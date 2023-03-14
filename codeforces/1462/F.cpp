cefa60061b7700b0379acb599587c6a612dc77ca
Mohit Varma <varmamohit122@gmail.com>
2023-03-15 02:08:00 +0530

    Add solution for problem `F - The Treasure of The Segments` Link: https://codeforces.com/contest/1462/problem/F Tags: binary search, data structures, greedy, *1800 Ref: https://codeforces.com/contest/1462/submission/197437326

--- codeforces/1462/F.cpp
+++ codeforces/1462/F.cpp
@@ -7,26 +7,59 @@ void solve()
 	int n;
 	cin >> n;
 
-	vector<pair<ll, ll>> a(n);
-	for (auto &i : a) {
-		cin >> i.first >> i.second;
+	vector<array<ll, 3>> a(n);
+	for (int i = 0; i < n; i++) {
+		ll l, r;
+		cin >> l >> r;
+		a[i] = {l, r, i};
 	}
+
+	sort(a.begin(), a.end(), [&](array<ll, 3> x, array<ll, 3> y) {
+		if (x[1] == y[1]) return x[0] < y[0];
+		return x[1] <= y[1];
+	});
+
+	vector<ll> nonOverlap(n, 0);
+
+	// get ranges that ends before and doesn't overlap
+	for (int i = 0; i < n; i++) {
+		ll l = 0, r = i - 1, mergeIndex = i;
+		while (l <= r) {
+			ll mid = l + (r - l) / 2;
+			if (a[mid][1] >= a[i][0]) {
+				mergeIndex = mid;
+				r = mid - 1;
+			}
+			else {
+				l = mid + 1;
+			}
+		}
+
+		nonOverlap[a[i][2]] += (mergeIndex);
+	}
+
 	sort(a.begin(), a.end());
 
-	vector<ll> starts(n), ends(n);
+	// get ranges that ends after and doesn't overlap
 	for (int i = 0; i < n; i++) {
-		starts[i] = a[i].first;
-		ends[i] = a[i].second;
+		ll l = i + 1, r = n - 1, mergeIndex = i;
+		while (l <= r) {
+			ll mid = l + (r - l) / 2;
+			if (a[mid][0] <= a[i][1]) {
+				mergeIndex = mid;
+				l = mid + 1;
+			}
+			else {
+				r = mid - 1;
+			}
+		}
+
+		nonOverlap[a[i][2]] += (n - 1 - mergeIndex);
 	}
 
-	sort(starts.begin(), starts.end());
-	sort(ends.begin(), ends.end());
-
-	int ans = INT_MAX;
+	ll ans = n + 10;
 	for (int i = 0; i < n; i++) {
-		int cnt = (int)(lower_bound(ends.begin(), ends.end(), a[i].first) - ends.begin());
-		cnt += (int)(starts.end() - upper_bound(starts.begin(), starts.end(), a[i].second));
-		ans = min(ans, cnt);
+		ans = min(ans, nonOverlap[i]);
 	}
 
 	cout << ans << '\n';
